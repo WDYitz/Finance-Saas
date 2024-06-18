d754b6ea5ebaa75551e406a959897a21385bbaf3
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-06-18 17:14:49 -0300

    feat: add clerk-auth middleware

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,25 +1,25 @@
-import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { z } from "zod";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
+app.get('/hello', clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({
+      error: 'Unauthorized'
+    })
+  }
   return c.json({
     message: 'Hello Next.js!',
-  });
-}).get('/hello/:name', zValidator("param", z.object({
-  name: z.string()
-})), (c) => {
-  const text = c.req.valid("param");
-
-  return c.json({
-    message: `Hello ${text}!`,
+    userId: auth.userId
   })
 });
+
 
 export const GET = handle(app);
 export const POST = handle(app);
