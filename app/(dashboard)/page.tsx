f6a965066a76a4ce6ad6fbff7c69ddb62d04874a
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-06-19 14:15:15 -0300

    feat: add tanstack query library

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
-export default function Home() {
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Loader2 } from "lucide-react";
+
+const Home = () => {
+  const { data: accounts, isLoading } = useGetAccounts();
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loader2 className="size-8 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserButton afterSignOutUrl="/" />
+      {accounts?.map((account) => (
+        <div key={account.id}>{account.name}</div>
+      ))}
     </main>
   );
-}
+};
+
+export default Home;
